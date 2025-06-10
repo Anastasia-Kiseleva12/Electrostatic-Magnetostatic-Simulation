69c94f96f6a7e4c6fd050cddcac2f3bc6befedc9
Anastasia-Kiseleva12 <pm15.kiseleva1@gmail.com>
2025-06-10 20:46:33 +0300

    first build and begining application

---
+++ ElectroMagSimulator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace ElectroMagSimulator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string[] SimulationModes { get; } = new[] { "Электростатика", "Магнитостатика" };
+
+        private string _selectedMode = "Электростатика";
+        public string SelectedMode
+        {
+            get => _selectedMode;
+            set => this.RaiseAndSetIfChanged(ref _selectedMode, value);
+        }
+
+        private double _areaWidth = 10;
+        public double AreaWidth
+        {
+            get => _areaWidth;
+            set => this.RaiseAndSetIfChanged(ref _areaWidth, value);
+        }
+
+        private double _areaHeight = 10;
+        public double AreaHeight
+        {
+            get => _areaHeight;
+            set => this.RaiseAndSetIfChanged(ref _areaHeight, value);
+        }
+
+        private double _sourceValue = 1.0;
+        public double SourceValue
+        {
+            get => _sourceValue;
+            set => this.RaiseAndSetIfChanged(ref _sourceValue, value);
+        }
+
+        private double _materialProperty = 1.0;
+        public double MaterialProperty
+        {
+            get => _materialProperty;
+            set => this.RaiseAndSetIfChanged(ref _materialProperty, value);
+        }
+
+        public ICommand BuildGridCommand { get; }
+        public ICommand SolveCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            BuildGridCommand = ReactiveCommand.Create(BuildGrid);
+            SolveCommand = ReactiveCommand.Create(Solve);
+            ClearCommand = ReactiveCommand.Create(Clear);
+        }
+
+        private void BuildGrid() { /* TODO */ }
+
+        private void Solve() { /* TODO */ }
+
+        private void Clear() { /* TODO */ }
+    }
+}
